b7da9f370848cf325b41ffa83cb5f333caf8e729
Eliel1210 <elielcolombo12@outlook.com>
2025-08-04 12:12:08 -0600

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHÉ_ÑAME = 'placas-caché-v40'; //  cambia el número en cada nueva versión
+const CACHÉ_ÑAME = 'placas-caché-v41'; //  cambia el número en cada nueva versión
 const urlsToCache = ['.', 'index.html', 'manifest.json', 'icon.png'];
 
 self.addEventListener('install', e => {
